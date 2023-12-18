a871acbe3bb6e88778836c9af6a28e50131a09d7
DanielsGHIES <daniel.guipel@educa.jcyl.es>
2023-12-18 20:42:57 +0100

    principio

---
+++ src/main/java/com/mycompany/iniciogit/InicioGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iniciogit;
+
+/**
+ *
+ * @author DANIELGUITIERREZPELA
+ */
+public class InicioGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=0;
+    }
+}
